c98a5a11e8b13b148e61f2d2adebdd4079ff7880
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-07 15:12:38 +0330

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -87,7 +87,7 @@ function createAddToCartButton(food, buyBtn , foodImgElem) {
   addToCartBtn.append(buySvgElem, btnText);
   buyBtn.innerHTML = "";
   buyBtn.append(addToCartBtn);
-
+// create add Event Listener
   addToCartBtn.addEventListener("click", function () {
     let count = 1;
 
