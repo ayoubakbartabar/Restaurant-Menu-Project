0df5371b6064a59bc915e38a67f2dc55416a4cda
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-08 23:34:58 +0330

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -299,67 +299,17 @@ function orderCartGenerator(foodList) {
   foodsCountNumberElem.innerHTML = ` (${foodList.length})`;
 }
 
-// // create function for remove item in order
-// function removeOrderFunc(foodId) {
-//   // set filter method
-//   addFoodsArray = addFoodsArray.filter((food) => {
-//     return food.id != foodId;
-//   });
-//   // call functions
-//   emptyVersion(addFoodsArray);
-//   orderCartGenerator(addFoodsArray);
-//   orderCalcu(addFoodsArray);
-// }
-// تابع برای حذف آیتم از سبد خرید
+// create function for remove item in order
 function removeOrderFunc(foodId) {
-  // حذف غذا از آرایه سبد خرید
-  addFoodsArray = addFoodsArray.filter((food) => food.id != foodId);
-
-  // به روز رسانی سبد خرید
-  orderCartGenerator(addFoodsArray);
-
-  // محاسبه مجدد قیمت
-  orderCalcu(addFoodsArray);
-
-  // نمایش یا پنهان کردن پیام سبد خرید خالی
+  // set filter method
+  addFoodsArray = addFoodsArray.filter((food) => {
+    return food.id != foodId;
+  });
+  // call functions
   emptyVersion(addFoodsArray);
-
-  // به روز رسانی دکمه‌ها و ورودی‌ها
-  updateAddToCartButton(foodId);
+  orderCartGenerator(addFoodsArray);
+  orderCalcu(addFoodsArray);
 }
-
-function updateAddToCartButton(foodId) {
-  const food = addFoodsArray.find((food) => food.id === foodId); // پیدا کردن غذا بر اساس آیدی
-  const foodBoxElem = document.querySelector(`.foods-box[data-id='${foodId}']`); // پیدا کردن المان غذا
-  const buyBtn = foodBoxElem.querySelector(".buying"); // گرفتن دکمه خرید
-  const foodImgElem = foodBoxElem.querySelector(".foods-img"); // گرفتن تصویر غذا
-
-  // پنهان کردن ورودی‌ها و دکمه‌ها
-  let quantityBox = foodBoxElem.querySelector(".quantity-box");
-  if (quantityBox) {
-    quantityBox.style.display = "none"; // پنهان کردن باکس تعداد
-  }
-
-  // ساخت دکمه "Add to Cart"
-  let newAddToCartBtn = document.createElement("button");
-  newAddToCartBtn.classList.add("add-to-cart");
-
-  let newBuySvgElem = document.createElement("span");
-  newBuySvgElem.classList.add("cart-icon");
-  newBuySvgElem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>`;
-
-  let newBtnText = document.createElement("span");
-  newBtnText.textContent = "Add to Cart";
-
-  newAddToCartBtn.append(newBuySvgElem, newBtnText);
-  buyBtn.innerHTML = ""; // حذف محتوای قبلی دکمه خرید
-  buyBtn.append(newAddToCartBtn); // اضافه کردن دکمه جدید به دکمه خرید
-  buyBtn.style.backgroundColor = ""; // پس زمینه را به حالت اولیه باز می‌گرداند
-
-  // بازسازی دکمه "Add to Cart"
-  createAddToCartButton(food, buyBtn, foodImgElem); // بازسازی دکمه "Add to Cart"
-}
-
 
 // create function for calculator order
 function orderCalcu(foodList) {
